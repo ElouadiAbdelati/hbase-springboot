3108bf921f2bd8349dba28425ae56b823d3bd72c
EL ouadi Abdelati <Elouadi_Abdelati@outlook.fr>
2021-12-16 15:17:00 +0100

    init

---
+++ src/main/java/com/quick/hbase/HbaseApplication.java
@@ -0,0 +1,24 @@
+package com.quick.hbase;
+
+import com.quick.hbase.config.HBaseClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HbaseApplication implements CommandLineRunner {
+	public static void main(String[] args) {
+		SpringApplication.run(HbaseApplication.class);
+	}
+
+	@Autowired
+	HBaseClient hBaseClient;
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		hBaseClient.createTable("annonce","ele");
+	}
+}
---
+++ src/main/java/com/quick/hbase/config/HbaseConfig.java
@@ -0,0 +1,29 @@
+package com.quick.hbase.config;
+
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.Set;
+
+@Configuration
+@EnableConfigurationProperties(HbaseProperties.class)
+public class HbaseConfig {
+	private final HbaseProperties properties;
+
+	public HbaseConfig(HbaseProperties properties) {
+		System.setProperty("hadoop.home.dir", "C:/winutils");
+		this.properties = properties;
+	}
+
+	public org.apache.hadoop.conf.Configuration configuration() {
+		org.apache.hadoop.conf.Configuration configuration = HBaseConfiguration.create();
+		Map<String, String> config = properties.getConfig();
+		Set<String> keySet = config.keySet();
+		for (String key : keySet) {
+			configuration.set(key, config.get(key));
+		}
+		return configuration;
+	}
+}
